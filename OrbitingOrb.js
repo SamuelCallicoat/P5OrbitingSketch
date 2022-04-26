b4f23f07a47da68eac4bc21e8d3b751389732682
SamuelCallicoat <samueleklundcallicoat@gmail.com>
2022-04-26 09:59:24 +0200

    added an origin point for OrbitingOrb objects, allowing for nesting orbits

--- OrbitingOrb.js
+++ OrbitingOrb.js
@@ -1,14 +1,19 @@
 class OrbitingOrb {
-  constructor(trigArrayPos, magnitude) {
+  constructor(trigArrayPos, magnitude, originObject) {
     this.x = sin(trigArrayPos * TWO_PI) * magnitude;
     this.y = cos(trigArrayPos * TWO_PI) * magnitude;
     this.originalTrigPos = trigArrayPos * TWO_PI;
     this.magnitude = magnitude;
+
+    this.originObject = originObject;
   }
 
   update(time) {
-    this.x = sin(this.originalTrigPos + time) * this.magnitude;
-    this.y = cos(this.originalTrigPos + time) * this.magnitude;
+    this.x = this.originObject.x;
+    this.y = this.originObject.y;
+
+    this.x += sin(this.originalTrigPos + time) * this.magnitude;
+    this.y += cos(this.originalTrigPos + time) * this.magnitude;
   }
 
   draw(size) {
